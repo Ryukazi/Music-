818adcad7bbd63e6f0d80e1f5316fc703dcf0f10
Ryukazi <158026107+Ryukazi@users.noreply.github.com>
2024-10-26 12:22:04 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = 'YOUR_YOUTUBE_API_KEY';
+const API_KEY = 'AIzaSyCNLVZKTIPdt3_9-TLQ76952RzGFeeX_n8';
 
 function searchSong() {
   const query = document.getElementById("searchQuery").value;
