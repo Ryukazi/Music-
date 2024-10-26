8fbba53822a9401f934b1d425c19243831ca2d47
Ryukazi <158026107+Ryukazi@users.noreply.github.com>
2024-10-26 12:13:53 +0545

    Update and rename app.js to script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+const API_KEY = 'YOUR_YOUTUBE_API_KEY';
+
+function searchSong() {
+  const query = document.getElementById("searchQuery").value;
+  if (!query) return;
+
+  fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&key=${API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+      const videoId = data.items[0]?.id?.videoId;
+      if (videoId) {
+        playVideo(videoId);
+      } else {
+        alert("No videos found");
+      }
+    })
+    .catch(error => console.error("Error fetching video:", error));
+}
+
+function playVideo(videoId) {
+  const videoPlayer = document.getElementById("videoPlayer");
+  videoPlayer.innerHTML = `<iframe width="100%" height="315" src="https://www.youtube.com/embed/${videoId}?autoplay=1" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`;
+}
